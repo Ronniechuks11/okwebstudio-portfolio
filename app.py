ffa422713ae855bf2bbabed1e0efe363f86379eb
ronniechuks11 <ronniechuks11@gmail.com>
2026-07-23 12:12:21 +0100

    Use Render environment variables

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, flash, redirect
 from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
-app.secret_key = "okzwebstudio"
+
+app.secret_key = os.getenv("SECRET_KEY", "okzwebstudio")
+
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "okzwebstudio@gmail.com"
-app.config["MAIL_PASSWORD"] = "htgclzpsxpjhntai"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
 
 mail = Mail(app)
-app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
 
 @app.route("/")
 def home():
