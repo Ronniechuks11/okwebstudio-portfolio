05574b6041b6cd9f53fa683f415fe748f8692da4
ronniechuks11 <ronniechuks11@gmail.com>
2026-07-23 11:33:19 +0100

    Update Latest Changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "okzwebstudio@gmail.com"
 app.config["MAIL_PASSWORD"] = "htgclzpsxpjhntai"
-app.config["MAIL_PASSWORD"] = "htgclzpsxpjhntai"
+
 mail = Mail(app)
 app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
 
