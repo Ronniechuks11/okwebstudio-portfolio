96845fcb92e6fd619eff1390cb09a7ae8ae413e3
ronniechuks11 <ronniechuks11@gmail.com>
2026-07-23 13:00:43 +0100

    Update Latest Changes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.secret_key = os.getenv("SECRET_KEY", "okzwebstudio")
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
